c14bdbeaa42750c8bdf9642f5e7a0c54e06a3ee2
Artamonov Aleksandr <artamonov.a@alkor.co.ru>
2023-12-26 23:07:20 +0500

    task-1 add jwt auth

--- utilitybill/src/test/java/in/artamonov/utilitybill/UtilityBillApplicationTests.java
+++ utilitybill/src/test/java/in/artamonov/utilitybill/UtilityBillApplicationTests.java
@@ -1,8 +1,9 @@
 package in.artamonov.utilitybill;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UtilityBillApplicationTests {
+class UtilitybillApplicationTests {
 
 }
